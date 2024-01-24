ab1fb36a3425aa3a8d945268eb5293eb5269a785
wujianbiao <18392371793@163.com>
2024-01-24 18:17:30 +0800

    两两反转链表的递归实现

--- src/main/java/org/wujianbiao/leetCodeTraining/leetcode/editor/cn/SwapNodesInPairs.java
+++ src/main/java/org/wujianbiao/leetCodeTraining/leetcode/editor/cn/SwapNodesInPairs.java
@@ -62,26 +62,67 @@ public class SwapNodesInPairs {
 
             // 迭代
             // 虚拟头节点
-            ListNode pre = new ListNode(-1);
-            pre.next = head;
-            ListNode cur = pre;
-            // 两个节点为一组最小的执行单元。如果当前节点为 null，或只剩一个节点时，则不需要再反转
-            while (cur.next != null && cur.next.next != null) {
-                ListNode start = cur.next;
-                ListNode end = cur.next.next;
+            // ListNode pre = new ListNode(-1);
+            // pre.next = head;
+            // ListNode cur = pre;
+            // // 两个节点为一组最小的执行单元。如果当前节点为 null，或只剩一个节点时，则不需要再反转
+            // while (cur.next != null && cur.next.next != null) {
+            // ListNode start = cur.next;
+            // ListNode end = cur.next.next;
+            //
+            // // 在每一次执行单元中，交换 start 和 end
+            // start.next = end.next;
+            // end.next = start;
+            // cur.next = end;
+            //
+            // // 交换完 start 和 end 之后，不要忘记将 cur 指到 start 上。这里一定要画个图示意一下，其实 start 就是每一个执行单元的最后一个节点。
+            // // 这个节点进入下一轮 while 循环，cur.next 和 cur.next.next 就是新一轮的 start 和 end
+            // cur = start;
+            // }
+            //
+            // // 交换过程中 pre 虚拟指针一直没变，始终指向头节点，因此直接返回即可。
+            // return pre.next;
 
-                // 在每一次执行单元中，交换 start 和 end
-                start.next = end.next;
-                end.next = start;
-                cur.next = end;
-
-                // 交换完 start 和 end 之后，不要忘记将 cur 指到 start 上。这里一定要画个图示意一下，其实 start 就是每一个执行单元的最后一个节点。
-                // 这个节点进入下一轮 while 循环，cur.next 和 cur.next.next 就是新一轮的 start 和 end
-                cur = start;
+            if (head == null || head.next == null) {
+                return head;
             }
 
-            // 交换过程中 pre 虚拟指针一直没变，始终指向头节点，因此直接返回即可。
-            return pre.next;
+            ListNode end = head.next;
+            // 递归的参数是 next.next，因为两个节点是一个反转的最小单元
+            // 每一个最小的递归单元中，head 等价于 start 节点，next 等价于 end 节点，end 需要反转为指向 start，
+            // 核心：start 需要指向的是下一个递归单元反转之后的第一个节点，也就是 end。所以需要使用 temp 来接收递归的返回值，使得 head 指向下一环的第一个节点。
+            ListNode temp = swapPairs2(end.next);
+
+            end.next = head;
+            head.next = temp;
+
+            return end;
+        }
+
+        /**
+         * 递归实现
+         * 终止条件：当前节点为空 或 下一个节点为空
+         * 最小单元内的操作：【start，end】节点的指向进行反转，end 指向 start，start 指向下一个单元的【起点】
+         * 返回值：反转之后的最小单元的【起点】
+         * 
+         * @param start
+         * @return
+         */
+        public ListNode swapPairs2(ListNode start) {
+            if (start == null || start.next == null) {
+                return start;
+            }
+
+            ListNode end = start.next;
+            // 递归的参数是 next.next，因为两个节点是一个反转的最小单元
+            // 每一个最小的递归单元中，head 等价于 start 节点，next 等价于 end 节点，end 需要反转为指向 start，
+            // 核心：start 需要指向的是下一个递归单元反转之后的第一个节点，也就是 end。所以需要使用 temp 来接收递归的返回值，使得 head 指向下一环的第一个节点。
+            ListNode temp = swapPairs2(end.next);
+
+            end.next = start;
+            start.next = temp;
+
+            return end;
         }
     }
     // leetcode submit region end(Prohibit modification and deletion)
