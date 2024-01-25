82af69c38642881cca03a814bc71db6a03e6372a
wujianbiao <18392371793@163.com>
2024-01-25 15:08:57 +0800

    相交链表的实现

--- src/main/java/org/wujianbiao/leetCodeTraining/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
+++ src/main/java/org/wujianbiao/leetCodeTraining/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -63,7 +63,28 @@ public class RemoveNthNodeFromEndOfList {
             // 1.遍历两次，第一次获取链表的长度，第二次直接删除。
             // 2.压栈，弹出到 n 个时候直接抛弃
             // 3.快慢指针，快指针先走 n 个，然后快慢指针同时开始走，直到快指针走到头。注意：慢指针是放 head 呢，还是放 head 前一位呢？画图就知道了
-            return null;
+            if (head == null) {
+                return head;
+            }
+
+            ListNode pre = new ListNode(0);
+            pre.next = head;
+
+            ListNode fast = pre;
+            for (int i = 0; i < n; i++) {
+                fast = fast.next;
+            }
+
+            ListNode slow = pre;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            // 此时 fast 已经走到最后一个节点，slow 正好在第 n-1 个节点
+            slow.next = slow.next.next;
+
+            return pre.next;
         }
     }
     // leetcode submit region end(Prohibit modification and deletion)
